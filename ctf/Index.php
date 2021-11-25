641d722636008bd6f9d6ac446715ec580ac23b96
c0del1ar <aryakrishna436@gmail.com>
2021-11-25 23:44:27 +0700

    UI fixes

--- ctf/Index.php
+++ ctf/Index.php
@@ -26,31 +26,6 @@ $servername   = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER
   <div class="container-fluid">
     <header>
       <h5><a class="navbar-brand" href="#"><?=$web_title?></a></h5>
-      <script>
-        function setTheme(themeName) {
-            localStorage.setItem('theme', themeName);
-            document.documentElement.className = themeName;
-        }
-
-        function toggleTheme() {
-            if (localStorage.getItem('theme') === 'theme-crack') {
-                setTheme('theme-hack');
-            } else {
-                setTheme('theme-crack');
-            }
-        }
-
-        (function () {
-            var slide = document.getElementById('slider');
-            if (localStorage.getItem('theme') === 'theme-crack') {
-                setTheme('theme-crack');
-                slide.checked = false;
-            } else {
-                setTheme('theme-hack');
-                slide.checked = true;
-            }
-        })();
-      </script>
       <label id="switcher" class="switcher mt-3">
         <input type="checkbox" onchange="toggleTheme()" id="slider">
         <span class="slider round"></span>
@@ -73,41 +48,47 @@ $servername   = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER
     </header>
     <div class="tab-content" id="myTabContent">
       <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-        <script>
-        $(document).ready(function(){
-          $.getJSON("data.json", function(data){
-            var logo = `<div class="card mt-3">
-              <img src=`+data.home.theLogo+`" class="card-img-top" alt="Logo">
-            </div>`;
-            $('#home').append(logo);
-            $.each(data.home.content, function(key, value){
-              var datas = `<div class="card mt-3 mb-3">
-                <div class="card-header">`+value.headerContent+`</div>
-                <div class="card-body">
-                  <h5 class="card-title">`+value.cardTitle+`</h5>
-                  <p class="card-text">`+value.textParagraf+`</div>
-                </div>
-              </div>`;
-              $('#home').append(datas);
-            });
-          });
-        });
-        </script>
       </div>
       <div class="tab-pane fade show" id="challenge" role="tabpanel" aria-labelledby="challenge-tab">
         <div id="app-challenge"></div>
       </div>
-      <script>
+      <div class="tab-pane fade" id="login" role="tabpanel" aria-labelledby="login-tab">
+        <? require('Login.php'); ?>
+      </div>
+      <div class="tab-pane fade" id="register" role="tabpanel" aria-labelledby="register-tab">
+        <? require('Register.php') ?>
+      </div>
+    </div>
+   </div>
+   <script>
       $(document).ready(function (){
+        $.getJSON("data.json", function(data){
+          var logo = `<div class="card mt-3">
+            <img src=`+data.home.theLogo+`" class="card-img-top" alt="Logo">
+          </div>`;
+          $('#home').append(logo);
+          $.each(data.home.content, function(key, value){
+            var datas = `<div class="card mt-3 mb-3">
+              <div class="card-header">`+value.headerContent+`</div>
+              <div class="card-body">
+                <h5 class="card-title">`+value.cardTitle.toUpperCase()+`</h5>
+                <p class="card-text">`+value.textParagraf+`</div>
+              </div>
+            </div>`;
+            $('#home').append(datas);
+          });
+        });
+        
+        <?if ( isset($_SESSION['logged_in']) ) { ?>
         $.getJSON('data.json', function(data){
           var appchall = '';
           $.each(data.category, function(key, cat){
-            appchall += `<div class="card">
+            appchall += `<div class="card mt-2 mb-3 challenge-content">
               <div class="card-header text-center app-challenge">`+key.toUpperCase()+`</div>
             </div>
             <div class="row">`;
             $.each(cat, function(cat, challenge){
-              appchall += `<a class="link-challenge" href="#`+challenge[0]+`"><div class="col-md-3">
+              appchall += `<a class="link-challenge m-3" href="#`+challenge[0]+`"><div class="col-md-3">
                 <figure class="text-center">
                   <blockquote class="blockquote title-challenge">
                     <p>`+challenge[0]+`</p>
@@ -120,16 +101,43 @@ $servername   = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER
             $('#app-challenge').append(appchall);
           });
         });
+        <? } else { ?>
+        var content = `<div class="card mt-5">
+          <div class="card-header">Login dulu main kemudian</div>
+          <div class="card-body">
+            <h3 class="card-title">Belum Login !</h3>
+            <p class="card-text">Ada masalah saat memuat tantangan. Kemungkinan kamu belum Login untuk membuat sesi permainan kamu.</p>
+            <p class="card-text">Silahkan buka halaman Login atau Register terlebih dahulu jika belum mendapatkan nomor peserta.</p>
+          </div>
+        </div>`
+        $('#app-challenge').append(content);
+        <? } ?>
       });
+      
+      function setTheme(themeName) {
+            localStorage.setItem('theme', themeName);
+            document.documentElement.className = themeName;
+      }
+
+      function toggleTheme() {
+            if (localStorage.getItem('theme') === 'theme-crack') {
+                setTheme('theme-hack');
+            } else {
+                setTheme('theme-crack');
+            }
+      }
+
+      (function () {
+            var slide = document.getElementById('slider');
+            if (localStorage.getItem('theme') === 'theme-crack') {
+                setTheme('theme-crack');
+                slide.checked = false;
+            } else {
+                setTheme('theme-hack');
+                slide.checked = true;
+            }
+      })();
       </script>
-      <div class="tab-pane fade" id="login" role="tabpanel" aria-labelledby="login-tab">
-        <? require('Login.php'); ?>
-      </div>
-      <div class="tab-pane fade" id="register" role="tabpanel" aria-labelledby="register-tab">
-        <? require('Register.php') ?>
-      </div>
-    </div>
-   </div>
   <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
